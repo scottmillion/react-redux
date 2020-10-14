e3136ffdd9e5949b05ee0ddc3705932b2e5b8c7d
Scott Million <scottmillion@gmail.com>
2020-10-14 13:51:41 +0700

    finished project

--- src/index.js
+++ src/index.js
@@ -10,9 +10,26 @@ import ReactDOM from 'react-dom';
 // Create a react component
 // ===========================
 
+function getButtonText() {
+  return 'Click on me now!';
+}
+
 // example of function based component
 const App = () => {
-  return <div>Hi There!</div>
+  const buttonText = 'Click Me!';
+  const labelText = "Enter name:";
+  return (
+    <div>
+      <label className="label" htmlFor="name">{labelText}</label>
+      <input type="text" id="name"/>
+      {/* Typical HTML way to add inline styles. Won't work. Need JSX way */}
+      {/* <button style="background-color: blue; color: white;">Submit</button> */}
+      <button style= {{ backgroundColor: 'blue', color: 'white'}}>
+        {getButtonText()}
+      </button>
+      <button>{buttonText}</button>
+    </div>
+  );  
 };
 
 // ===========================
